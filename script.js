fa8e1b297dc63db52c28c907f0e2134728e8ebf4
Shaquon <shaquonkelley@gmail.com>
2022-10-17 22:26:39 -0400

    first commit. Added html with instructions. Added javascript with example code

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("1 Display the numbers from 1 to 20. (1, 2, 3, …,19, 20)");
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+};
+
+console.log("2 Display the even numbers from 1 to 20. (2, 4, 6, …, 18, 20)");
+// code goes here
+
+console.log("3 Display the odd numbers from 1 to 20. (1, 3, 5, …, 17, 19)");
+// code goes here
+
+console.log("4 Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)");
+// code goes here
+
+console.log("5 Display the square numbers up to 100. (1, 4, 9, …, 81, 100)");
+// code goes here
+
+console.log("6 Display the numbers counting backwards from 20 to 1. (20, 19, 18, …, 2, 1)");
+// code goes here
+
+console.log("8 Display the even numbers counting backwards from 20. (20, 18, 16, …, 4, 2)");
+// code goes here
+
+console.log("9 Display the odd numbers from 20 to 1, counting backwards. (19, 17, 15, …, 3, 1)");
+// code goes here
+
+console.log("10 Display the multiples of 5, counting down from 100. (100, 95, 90, …, 10, 5)");
+// code goes here
+
+console.log("Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)");
+// code goes here
